96ff63a674a6e50b0873e237c9981eb204172278
primaam <pmaharyono@gmail.com>
2024-08-12 12:07:31 +0700

    update

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,5 @@ import Header from "./Header/Header";
 import ContactMe from "./ContactMe/ContactMe";
 import Project from "./Project/Project";
 
+// will use this to import component
 export {AboutMe, Header, ContactMe, Project}
